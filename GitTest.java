1cb68f06d2e6d4c08e66c7dac04f20626cd32c1d
Tre Haga <thaga1@radford.edu>
2017-03-02 19:04:26 -0500

    Backwards
    Changed some stuff

---
+++ GitTest.java
@@ -0,0 +1,25 @@
+/**
+ * Created by DMan on 2/28/2017.
+ */
+public class GitTest {
+
+    public static void main(String[] args){
+
+        char[] pack = {'R','A','D','P', 'A','C','K'};
+
+        System.out.println("This is a Test");
+
+        int check = 10;
+        do{
+            System.out.println(check);
+            check--;
+        }while(check != 0);
+
+        System.out.println("We are the..");
+
+        for(int pos = pack.length - 1; pos >= 0 pos--){
+            System.out.println(pack[pos]);
+        }
+
+    }
+}
